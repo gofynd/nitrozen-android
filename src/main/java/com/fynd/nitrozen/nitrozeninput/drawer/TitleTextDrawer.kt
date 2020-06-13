67e84fe96845a8f4b12fae8656464729b61ea9f3
Deepak Gavkar <deepak.gavkar@gmail.com>
2020-06-13 22:02:50 +0530

    Dropdown issue resolved

--- src/main/java/com/fynd/nitrozen/nitrozeninput/drawer/TitleTextDrawer.kt
+++ src/main/java/com/fynd/nitrozen/nitrozeninput/drawer/TitleTextDrawer.kt
@@ -2,16 +2,16 @@ package com.fynd.nitrozen.nitrozeninput.drawer
 
 import android.graphics.Typeface
 import android.text.TextUtils
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
-import com.fynd.nitrozen.utils.Drawer
 import com.fynd.nitrozen.R
 import com.fynd.nitrozen.nitrozenbutton.utils.dpToPx
-import com.fynd.nitrozen.nitrozenbutton.utils.pxToDp
 import com.fynd.nitrozen.nitrozeninput.NInput
 import com.fynd.nitrozen.nitrozeninput.model.NitrozenInput
+import com.fynd.nitrozen.utils.Drawer
 
 
 class TitleTextDrawer(val view: NInput, val input: NitrozenInput) :
@@ -21,6 +21,7 @@ class TitleTextDrawer(val view: NInput, val input: NitrozenInput) :
 
     override fun draw() {
         init()
+        isViewAdded = true
         view.addView(tv)
     }
 
@@ -30,7 +31,10 @@ class TitleTextDrawer(val view: NInput, val input: NitrozenInput) :
 
     private fun init() {
         val params: LinearLayout.LayoutParams =
-            LinearLayout.LayoutParams( LinearLayout.LayoutParams.MATCH_PARENT,  LinearLayout.LayoutParams.WRAP_CONTENT)
+            LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
         tv.setSingleLine()
         tv.ellipsize = TextUtils.TruncateAt.END
         tv.setLines(1)
@@ -59,9 +63,18 @@ class TitleTextDrawer(val view: NInput, val input: NitrozenInput) :
             tv.setTypeface(tf, Typeface.NORMAL)
         } catch (e: Exception) {
         }
+        if (isReady()) {
+            tv.visibility = View.VISIBLE
+        } else {
+            tv.visibility = View.GONE
+        }
     }
 
-    override fun updateLayout() {
-        init()
+    override fun  updateLayout() {
+        if (isViewAdded) {
+            init()
+        } else {
+            draw()
+        }
     }
 }
