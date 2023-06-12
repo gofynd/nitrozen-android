ef0d22c57763f1e8ee7f0d5150cd6ac0fa01284a
Saurabh Patel <saurabhpatel@gofynd.com>
2023-06-12 08:57:45 +0530

    Customise Default checkbox to change customise radius

--- nitrozen-android-lib/src/main/java/com/fynd/nitrozen/components/checkbox/CheckBox.kt
+++ nitrozen-android-lib/src/main/java/com/fynd/nitrozen/components/checkbox/CheckBox.kt
@@ -3,7 +3,9 @@ package com.fynd.nitrozen.components.checkbox
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.LocalMinimumTouchTargetEnforcement
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
@@ -14,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.tooling.preview.Preview
+import com.fynd.nitrozen.components.checkbox.base.*
 import com.fynd.nitrozen.theme.NitrozenTheme
 import com.fynd.nitrozen.utils.extensions.clickableWithoutRipple
 
