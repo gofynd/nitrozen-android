e3fac1be6af460a3a7c8b21bff43e8b69fa06dc2
Saurabh Patel <saurabhpatel@gofynd.com>
2023-08-28 11:46:48 +0530

    Add leading and loading in outlined button

--- nitrozen-android-lib/src/main/java/com/fynd/nitrozen/components/button/outlined/OutlinedButton.kt
+++ nitrozen-android-lib/src/main/java/com/fynd/nitrozen/components/button/outlined/OutlinedButton.kt
@@ -3,13 +3,19 @@ package com.fynd.nitrozen.components.button.outlined
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.fynd.nitrozen.components.autosizetext.NitrozenAutoResizeText
+import com.fynd.nitrozen.components.autosizetext.NitrozenAutoResizeTextStyle
 import com.fynd.nitrozen.components.button.NitrozenButtonConfiguration
 import com.fynd.nitrozen.components.button.NitrozenButtonConfiguration.Default
 import com.fynd.nitrozen.components.button.NitrozenButtonStyle
@@ -42,12 +48,43 @@ private fun NitrozenOutlinedButton_Disabled() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+private fun NitrozenOutlinedButton_Leading() {
+    NitrozenTheme {
+        NitrozenOutlinedButton(
+            text = "Outlined Button Disabled",
+            onClick = {},
+            enabled = false,
+            leading = {
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+            }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun NitrozenOutlinedButton_Loading() {
+    NitrozenTheme {
+        NitrozenOutlinedButton(
+            text = "Outlined Button Disabled",
+            onClick = {},
+            enabled = false,
+            isLoading = true
+        )
+    }
+}
+
+
 @Composable
 fun NitrozenOutlinedButton(
     modifier: Modifier = Modifier,
     text: String,
     enabled: Boolean = true,
     onClick: () -> Unit,
+    leading: @Composable (() -> Unit)? = null,
+    isLoading: Boolean = false,
     style: NitrozenButtonStyle.Outlined = NitrozenButtonStyle.Outlined.Default,
     configuration: NitrozenButtonConfiguration.Outlined = NitrozenButtonConfiguration.Outlined.Default,
 ) {
@@ -75,10 +112,22 @@ fun NitrozenOutlinedButton(
             color = style.borderColor
         )
     ) {
-        Text(
-            text = text,
-            style = style.textStyle,
-            color = color,
-        )
+        if(isLoading){
+            CircularProgressIndicator(
+                color = style.borderColor,
+                strokeWidth = 4.dp,
+            )
+        }else {
+            if (leading != null) {
+                leading()
+            }
+            NitrozenAutoResizeText(
+                text = text,
+                style = NitrozenAutoResizeTextStyle(
+                    textStyle = style.textStyle,
+                    textColor = color,
+                )
+            )
+        }
     }
 }
